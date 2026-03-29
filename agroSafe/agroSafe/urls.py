622c26faa367ea6f9a8b2b06c5140cdfe1fd30ec
gabriel valomi <gabriel.valomi@gmail.com>
2026-03-29 17:44:26 -0300

    atualização
    criei um template basico e ja integrei, depois que os definitivos ficarem prontos sera mais facil para integrar e so trocar os arquivos e as urls, e deu um up nas funçoes de cadastros com mais alguns ifs

--- agroSafe/agroSafe/urls.py
+++ agroSafe/agroSafe/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'main/index.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('appbk.urls')),
+    path('', home),
 ]
